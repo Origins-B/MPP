d83a3fc7aa91735d8bc004223c1a93d6ac912fd7
MCmoefBD <moe_fd@aol.com>
2014-09-08 00:01:13 -0400

    First commit

---
+++ src/com/OriginsB/Main.java
@@ -0,0 +1,9 @@
+package com.OriginsB;
+
+public class Main {
+
+	public static void main(String[] args) {
+    Tile tile = new Tile(0, 0, 0);
+	}
+
+}
---
+++ src/com/OriginsB/Tile.java
@@ -0,0 +1,14 @@
+package com.OriginsB;
+
+public class Tile {
+	
+public static int ID;
+public static int XPos;
+public static int YPos;
+	
+public Tile(int ID, int XPos, int Ypos){
+	XPos = Tile.XPos;
+	ID = Tile.ID;
+	Ypos = Tile.YPos;
+}
+}
