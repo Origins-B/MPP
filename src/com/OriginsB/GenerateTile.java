ea89b0c24c6925f47cca1737aad942869ea59a74
MCmoefBD <moe_fd@aol.com>
2014-09-22 21:16:35 -0400

    Removed unused files

--- src/com/OriginsB/GenerateTile.java
+++
@@ -1,8 +0,0 @@
-package com.OriginsB;
-
-public class GenerateTile {
-public static Tile generateTile(int ID,int Pos){
-	System.out.println("Generated Tile with ID "+ID+" with Pos: "+Pos);
-	return new Tile(ID,Pos);	
-}
-}
